a2f3de35b5b6c33490710d7be34a99deaadaecd2
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-12-25 18:08:26 +0200

    create function auth with data from user

--- index/index.php
+++ index/index.php
@@ -19,4 +19,37 @@ function UserExit() {
     return $isAuth = 0;
 }
 
+function authWithCredential($username, $password) {
+    $isAuth = 0;
+
+    $link = getConnection();
+    $login = mysqli_real_escape_string($link, $username);
+    $sql = "select id_user, login, pass from users where login = '$login'";
+    $user_date = getRowResult($sql, $link);
+
+    if ($user_date) {
+        $passHash = $user_date['pass'];
+        $id_user = $user_date['id_user'];
+        $idUserCookie = microtime(true) . rand(100, 100000000);
+        if (password_verify($password, $passHash)) {
+            $_SESSION['id_user'] = $id_user;
+            $_SESSION['IdUserSession'] = $idUserCookie;
+            $sql = "insert into users_auth (id_user, hash_cookie, date, prim) values ('$id_user', '$idUserCookie', now(), '1241241241')";
+            executeQuery($sql);
+            $isAuth = 1;
+
+            if ($_POST['rememberme']) {
+                setcookie('idUserCookie', $idUserCookie, time() + 3600 * 24 * 7);
+            }
+        } else {
+            UserExit()
+        }
+
+    } else {
+        UserExit()
+    }
+
+    return $isAuth;
+}
+
 ?>
